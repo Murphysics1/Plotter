37a0b4b1e1d0d15d748fae303db79dc5d4563fe4
Murphysics1 <88453737+Murphysics1@users.noreply.github.com>
2021-09-21 12:09:02 -0400

    Create Plotter.py

---
+++ Plotter.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 11:51:45 2021
+
+@author: mikem
+"""
+import matplotlib.pyplot as plt
+import tkinter as tk
+import seaborn as sns
+sns.set()
+
+root = tk.Tk()
+root.title("Plotter")
+root.geometry('500x500')
+root.resizable(width=False, height=False)
+
+def Load_File():
+    pass
+
+
+root.mainloop()
