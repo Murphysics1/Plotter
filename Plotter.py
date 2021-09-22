bd444b2e0ba87c31d2f354ca2b475ea1549bca6e
Murphysics1 <88453737+Murphysics1@users.noreply.github.com>
2021-09-22 02:26:05 -0400

    First Draft
    Exploring file import and automating analysis

--- Plotter.py
+++ Plotter.py
@@ -4,18 +4,65 @@ Created on Tue Sep 21 11:51:45 2021
 
 @author: mikem
 """
+import pandas as pd
 import matplotlib.pyplot as plt
 import tkinter as tk
+from tkinter import filedialog
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 import seaborn as sns
 sns.set()
 
-root = tk.Tk()
-root.title("Plotter")
-root.geometry('500x500')
-root.resizable(width=False, height=False)
 
 def Load_File():
-    pass
+    filename = filedialog.askopenfilename()
+    root.quit()
+    Make_Plot(filename)
 
+def Make_Plot(dataset):
+    data = pd.read_csv(dataset)
+    #data = list(csv.reader(load))
+    data.describe()
+    
+    x_label = data.columns[0]
+    y_label = data.columns[1]
+    x = data[x_label]
+    y = data[y_label]
+    
+    model, rmse,r2,y_pred = Model(x,y)
+    
+    xu = max(x)/20
+    yu = max(y)/20
+    
+    plt.scatter(x,y)
+    plt.plot(x,y_pred,c='orange')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.text(1*xu,19*yu,f'Slope: {model.coef_[0]:.2f}')
+    plt.text(1*xu,17*yu,f'Intercept: {model.intercept_:.2f}')
+    plt.text(1*xu,15*yu,f'R^2: {r2:.2f}')
+    plt.show()
+    
+    
+def Model(x,y):
+    
+    x = x.values.reshape(-1,1)
+    
+    model = LinearRegression()
+    model.fit(x,y)
+    y_pred = model.predict(x)
+
+    rmse = mean_squared_error(y, y_pred)
+    r2 = r2_score(y, y_pred)
+    
+    return model,rmse,r2, y_pred
+    
+root = tk.Tk()
+root.title("Plotter")
+root.geometry('100x50')
+root.resizable(width=False, height=False)
+
+button = tk.Button(root, text='Select Data', command=Load_File)
+button.pack()
 
 root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Plotter
- Create plots from csv files
+ Create simple scatter plots from csv files.  Use the simple GUI to select the file and a  plot will generate as well as a linear line of best fit. R^2, Slope, and Intercept  Information will also be displayed 
+ 
+ To Use:
+ 	In your CSV file:
+ 	Put axis labels into row A, 
+ 	X axis values into column 1 and Y axis values into column 2
+
+	In the Plotter.py:
+	Press the Select File button and select your file
+	The graph will generate automatically
+
